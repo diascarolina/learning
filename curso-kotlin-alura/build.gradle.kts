2c4d20765686d07d2f61ec64afe0fcbc55cedf0f
Carolina Dias <carolina.dias@creditas.com>
2022-10-31 15:23:45 -0300

    Update files

--- curso-kotlin-alura/build.gradle.kts
+++ curso-kotlin-alura/build.gradle.kts
@@ -1,11 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    application
+    kotlin("jvm") version "1.6.20"
 }
 
-group = "me.carolinaaraujodias"
+group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -17,13 +16,9 @@ dependencies {
 }
 
 tasks.test {
-    useJUnit()
+    useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
