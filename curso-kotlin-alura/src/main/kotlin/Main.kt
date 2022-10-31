0c9a8d869fe3f24b7c62bf3c2d7c13ebd30bbcc5
Carolina Dias <carolina.dias@creditas.com>
2022-10-31 18:05:40 -0300

    Update files

--- curso-kotlin-alura/src/main/kotlin/Main.kt
+++ curso-kotlin-alura/src/main/kotlin/Main.kt
@@ -1,6 +1,52 @@
 fun main() {
     println("Bem-vindo ao Bytebank")
-    val titular = "Alex"
 
-    println(titular)
+    var i = 0
+    while (i < 5){
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
+
+        println("Titular: $titular")
+        println("Número da Conta: $numeroConta")
+        println("Saldo: $saldo")
+        println()
+        i++
+    }
+
+//    for (i in 5 downTo 1) {
+//
+//        if (i == 4) {
+//            continue
+//        }
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo: Double = i + 10.0
+//
+//        println("Titular: $titular")
+//        println("Número da Conta: $numeroConta")
+//        println("Saldo: $saldo")
+//        println()
+//
+////    testaCondicoes(saldo)
+//
+//    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("Conta é positiva.")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra.")
+    } else {
+        println("Conta é negativa.")
+    }
+
+    // o if acima é equivalente ao else abaixo
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva.")
+        saldo == 0.0 -> println("Conta é neutra.")
+        else -> println("Conta é negativa.")
+    }
 }
